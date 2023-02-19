41738f650bfadec45caba1d62cea149996d54589
char5742 <f.shuuma@gmail.com>
2023-02-20 01:28:58 +0900

    feat: add export

--- src/Tetris.jl
+++ src/Tetris.jl
@@ -8,7 +8,7 @@ export action!, valid_movement, move, rotate
 include("move.jl")
 export MoveState
 include("game.jl")
-export GameState, put_mino!
+export GameState, put_mino!, check_tspin
 include("gui.jl")
 export draw_game, draw_game2file, init_screen, endwin
 end # module Tetris
