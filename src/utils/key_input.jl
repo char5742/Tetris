29a37fdb2e585e89eae11f43913b37c36b1715b2
char5742 <f.shuuma@gmail.com>
2023-02-20 16:36:45 +0900

    fix: Access is denied

--- src/utils/key_input.jl
+++ src/utils/key_input.jl
@@ -7,4 +7,7 @@ const VK_UP = 0x26
 const VK_RIGHT = 0x27
 const VK_DOWN = 0x28
 const VK_Z = 0x5A
-get_key_state(key::Symbol) = ccall((:getkeystate, joinpath(PROJECT_ROOT,"game.so")), Int32, (Int32,), eval(key))
+
+const _key_state_source= joinpath(PROJECT_ROOT,"game.so")
+chmod(_key_state_source, filemode(_key_state_source) | 0o755)
+get_key_state(key::Symbol) = ccall((:getkeystate, _key_state_source), Int32, (Int32,), eval(key))
