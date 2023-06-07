fac0fc6d70b380dcceadd28d1f995920fa68e8e1
char5742 <f.shuuma@gmail.com>
2023-06-07 13:22:40 +0900

    feat: actionでInt64をうけつけるように

--- src/component/action.jl
+++ src/component/action.jl
@@ -11,7 +11,8 @@ struct Action
     hard_drop::Bool
 end
 Action(x::Int8, y::Int8, rotate::Int8) = Action(x, y, rotate, false, false)
-
+Action(x::Int64, y::Int64, rotate::Int64) = Action(x |> Int8, y  |> Int8, rotate |> Int8, false, false)
+Action(x::Int64, y::Int64, rotate::Int64, hold::Bool, hard_drop::Bool) = Action(x |> Int8, y  |> Int8, rotate |> Int8, hold, hard_drop)
 struct Actionflow
     flow::Vector{Action}
     after_bord::Matrix{Int8}
