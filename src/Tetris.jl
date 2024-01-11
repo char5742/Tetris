15ad7642d65e2644831f71ca444c187a8d7626ec
char5742 <fujino@stellarcode.jp>
2024-01-12 00:18:29 +0900

    refactor: 命名を適切なものに

--- src/Tetris.jl
+++ src/Tetris.jl
@@ -9,19 +9,19 @@ export get_current_key_state, is_pushed
 include("utils/sleep.jl")
 export mysleep, sleep60fps
 
-include("component/direction_component.jl")
+include("components/direction_component.jl")
 using .Direction
-include("component/mino_component.jl")
-include("component/position_component.jl")
-include("component/action_component.jl")
-include("component/game_board_component.jl")
+include("components/mino_component.jl")
+include("components/position_component.jl")
+include("components/action_component.jl")
+include("components/game_board_component.jl")
 export AbstractMino, Mino, GameBoard, Position, Action,
     AbstractAction, HorizontalMoveAction, DownwardMoveAction, SoftDropAction, RotateAction,
     HoldAction, HardDropAction, EmptyAction, Actionflow
-include("srs.jl")
-export action!, move, rotate, is_valid_mino_movement
+include("actions.jl")
+export move, rotate, is_valid_mino_movement
 include("game.jl")
-export GameState, put_mino!, check_tspin, game_end!, get_ghost_position
+export GameState, action!, put_mino!, check_tspin, game_end!, get_ghost_position
 include("move.jl")
 export MoveState, reset_auto_set_delay_on_move!, fall!, put_mino!, process_das!
 include("gui.jl")
